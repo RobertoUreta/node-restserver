cd4081f11741b207f75a25fd76692619fe52f2f6
Roberto <roberto.ureta.14@gmail.com>
2018-10-26 23:11:35 -0300

    Google Sign-In

--- server/config/config.js
+++ server/config/config.js
@@ -36,3 +36,9 @@ process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
 //=============
 
 process.env.SEED = process.env.SEED || 'seed-desarrollo';
+
+//=============
+//GOOGLE CLIENT ID
+//=============
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || '1096114384771-6i579lov7iuv6o6t7pni35rs46gjgmi9.apps.googleusercontent.com';
